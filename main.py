2023af578319ac46c215530963d22c0dd2f7472d
sailaxmi01 <sailaxmi.25mip10126@vitbhopal.ac.in>
2025-11-21 19:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#Shopping  bill project
+print("------Shopping Bill Generator------")
+items=[]
+prices=[]
+quantities=[]
+n=int(input("Enter number of items you want to buy:"))
+for i in range(n):
+  print(f"\nItem {i+1}")
+  name= input("Enter item name:")
+  price=float(input("Enter price of the item:"))
+  qty=int(input("Enter quantity:"))
+  items.append(name)
+  prices.append(price)
+  quantities.append(qty)
+total=0
+for i in range(n):
+  total+= prices[i]*quantities[i]
+print("\n------ Final Bill ------")
+for i in range(n):
+  print(f"{items[i]} X {quantities[i]} = Rs{prices[i]*quantities[i]}")
+print("\nTotal Amount to Pay: Rs", total)
+print("--------------------------------------")
+    
